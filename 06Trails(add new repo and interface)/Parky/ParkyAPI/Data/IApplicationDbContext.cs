80ffa6c7e4156eb278f04078c4432af9e44c7f7c
Victor Avila <dev__game@outlook.com>
2021-08-26 22:43:01 -0500

    six topic ready

---
+++ 06Trails(add new repo and interface)/Parky/ParkyAPI/Data/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Models;
+
+namespace ParkyAPI.Data
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<T> Set<T>() where T : class;
+
+        int SaveChanges();
+    }
+}
---
+++ 06Trails(add new repo and interface)/Parky/ParkyAPI/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface INationalParkRepository<T> : IBaseRepository<T>
+    {
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+    }
+}
